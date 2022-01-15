9b1cf22d1727c275eb429aa1c4c19ecfa04706d6
Ryan Tse <rytse@umd.edu>
2022-01-14 22:14:03 -0500

    Create MPE env with covering objective
    Like simple_spread_v2, but instead of going to N different
    landmarks, there's one big sphere and you want to maximize
    surface area coverage.
    
    The weird directory structure with the indirection is to
    follow the PettingZoo conventions.

---
+++ envs/swarmcover/scenarios/swarm_cover.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+from pettingzoo.mpe._mpe_utils.core import Agent, World
+#from pettingzoo.mpe._mpe_utils.scenario import BaseScenario
+from pettingzoo.mpe.scenarios.simple_spread import Scenario as SimpleSpreadScenario
+#from core import Target
+
+class Scenario(SimpleSpreadScenario):
+    def make_world(self, N=3, AGENT_SIZE=0.15, LANDMARK_SIZE=1.5, N_THETA=10):
+        world = World()
+        # set any world properties first
+        world.dim_c = 2
+        num_agents = N
+        num_landmarks = 1   # swarm cover only wants to cover one target
+        world.collaborative = True
+        # add agents
+        world.agents = [Agent() for i in range(num_agents)]
+        for i, agent in enumerate(world.agents):
+            agent.name = f'agent_{i}'
+            agent.collide = True
+            agent.silent = True
+            agent.size = AGENT_SIZE
+        # add landmarks
+        world.landmarks = [Landmark() for i in range(num_landmarks)]
+        for i, landmark in enumerate(world.landmarks):
+            landmark.name = 'landmark %d' % i
+            landmark.size = LANDMARK_SIZE 
+            landmark.collide = True  # you can bounce off if you get too close
+            landmark.movable = False
+
+        # Generate mesh that approximates the target
+        theta = np.linspace(0, 2*np.pi, N_THETA)
+        xx = LANDMARK_SIZE * np.cos(theta)
+        yy = LANDMARK_SIZE * np.sin(theta)
+        self.target_mesh = np.rollaxis(np.array([xx, yy]), 1)
+
+        return world
+
+
+    def global_reward(self, world):
+        rew = 0
+        in_place_mesh = self.target_mesh - world.landmarks[0].state.p_pos 
+
+        for a in world.agents:
+            rew -= np.min(np.norm(in_place_mesh - a.state.p_pos))
+
+        return rew
---
+++ envs/swarmcover/swarm_cover_v1.py
@@ -0,0 +1,15 @@
+from pettingzoo.utils.conversions import parallel_wrapper_fn
+from pettingzoo.mpe._mpe_utils.simple_env import SimpleEnv, make_env
+from .scenarios.swarm_cover import Scenario
+
+class raw_env(SimpleEnv):
+    def __init__(self, N=3, local_ratio=0.5, max_cycles=25, continuous_actions=False):
+        assert 0. <= local_ratio <= 1., "local_ratio is a proportion. Must be between 0 and 1."
+        scenario = Scenario()
+        world = scenario.make_world(N)
+        super().__init__(scenario, world, max_cycles, continuous_actions, local_ratio)
+        self.metadata['name'] = "swarm_cover_v1"
+
+
+env = make_env(raw_env)
+parallel_env = parallel_wrapper_fn(env)
