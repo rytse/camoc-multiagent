0a3af85d8cef7ba4b6ddd57e68362d347720faf6
EliLichtblau <elilichtblau@gmail.com>
2022-01-17 21:25:44 -0500

    most things

--- camoc_multiagent/envs/custom_env/fast_simple_spread.py
+++ camoc_multiagent/envs/custom_env/fast_simple_spread.py
@@ -2,120 +2,10 @@ from pettingzoo.mpe._mpe_utils.simple_env import SimpleEnv
 from pettingzoo.utils.env import AECEnv
 import numpy as np
 from gym.utils import seeding
-from typing import Optional, int
+from typing import Optional, int, List
 from itertools import combinations
 from scipy.spatial import distance_matrix
 
-class FastSimpleEnv(AECEnv):
-    def __init__(self, scenario, world, max_cycles, continuous_actions=False, local_ratio=None):
-        super().__init__()
-        self.seed()
-        self.max_cycles = max_cycles
-        self.scenario = scenario
-        self.world = world
-        self.continuous_actions = continuous_actions
-        self.local_ratio = local_ratio
-
-        self.scenario.reset_world(self.world, self.np_random)
-        
-        self.action_spaces = None 
-        self.observation_spaces = None
-
-        # Initialize spaces is important 
-
-    def observation_space(self, agent: int):
-        raise Exception("Observation space funciton not implemented")
-
-
-    def action_spaces(self, agent: int):
-        raise Exception("Action space funciton not implemented not implemented")
-    
-    def seed(self, seed: Optional[int] = None):
-        self.np_random, seed = seeding.np_random(seed)
-
-    
-    def observe(self, agent: int):
-        return self.scenario.observation(agent, self.world)
-    
-    def state(self):
-        raise Exception("State not implented yet...")
-    
-    def reset(self):
-        self.scenario.reset_world(self.world, self.np_random)
-
-        
-
-
-
-    def render(self, mode='human'):
-        from . import rendering
-
-        if self.viewer is None:
-            self.viewer = rendering.Viewer(700, 700)
-
-        # create rendering geometry
-        if self.render_geoms is None:
-            # import rendering only if we need it (and don't import for headless machines)
-            # from gym.envs.classic_control import rendering
-            # from multiagent._mpe_utils import rendering
-            self.render_geoms = []
-            self.render_geoms_xform = []
-            for entity in self.world.entities:
-                geom = rendering.make_circle(entity.size)
-                xform = rendering.Transform()
-                if 'agent' in entity.name:
-                    geom.set_color(*entity.color[:3], alpha=0.5)
-                else:
-                    geom.set_color(*entity.color[:3])
-                geom.add_attr(xform)
-                self.render_geoms.append(geom)
-                self.render_geoms_xform.append(xform)
-
-            # add geoms to viewer
-            self.viewer.geoms = []
-            for geom in self.render_geoms:
-                self.viewer.add_geom(geom)
-
-            self.viewer.text_lines = []
-            idx = 0
-            for agent in self.world.agents:
-                if not agent.silent:
-                    tline = rendering.TextLine(self.viewer.window, idx)
-                    self.viewer.text_lines.append(tline)
-                    idx += 1
-
-        alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        for idx, other in enumerate(self.world.agents):
-            if other.silent:
-                continue
-            if np.all(other.c == 0):
-                word = '_'
-            elif self.continuous_actions:
-                word = '[' + ",".join([f"{comm:.2f}" for comm in other.c]) + "]"
-            else:
-                word = alphabet[np.argmax(other.c)]
-
-            message = (other.name + ' sends ' + word + '   ')
-
-            self.viewer.text_lines[idx].set_text(message)
-
-        # update bounds to center around agent
-        all_poses = [entity.pos for entity in self.world.entities]
-        cam_range = np.max(np.abs(np.array(all_poses))) + 1
-        self.viewer.set_max_size(cam_range)
-        # update geometry positions
-        for e, entity in enumerate(self.world.entities):
-            self.render_geoms_xform[e].set_translation(*entity.pos)
-        # render to display or array
-        return self.viewer.render(return_rgb_array=mode == 'rgb_array')
-
-class raw_env(FastSimpleEnv):
-    def __init__(self, N=5, local_ratio=0.25, max_cycles=25, continuous_actions=True):
-        assert 0. <= local_ratio <= 1., "local_ratio is a proportion. Must be between 0 and 1."
-        scenario = FastScenario()
-        world = scenario.make_world(N)
-        super().__init__(scenario, world, max_cycles, continuous_actions, local_ratio)
-        self.metadata['name'] = "swarm_cover_v1"
 
 
 
@@ -184,11 +74,24 @@ class FastWorld:
         self.n_entities = n_entities
         self.entity_sizes = entity_sizes
 
-        self.positions: np.ndarray = np.zeros(shape=(n_entities, 2))
-        self.velocities: np.ndarray = np.zeros(shape=(n_entities, 2))
+        # World parameters
+        self.dt = 0.1
+        self.damping = 0.25
+
+        self.dim_p = 2 # (x,y)
+        self.contact_force = np.float32(1e2)
+        self.contact_margin = np.float32(1e-3)
+
+
+        # Agent controls, every agent has an X, Y positon
+        self.positions: np.ndarray = np.zeros(shape=(n_entities, self.dim_p))
+        self.velocities: np.ndarray = np.zeros(shape=(n_entities, self.dim_p))
 
         self.ctrl_thetas: np.ndarray = np.zeros(n_entities)
         self.ctrl_speeds: np.ndarray = np.zeros(n_entities)
+
+        # Agent can do an action, action space for each agent is (X, Y)
+        self.agent_actions: np.ndarray[np.ndarray] = np.zeros(shape=(n_entities, self.dim_p))
 
         # Only agents are movable
         self.movables = np.zeros(n_entities)
@@ -199,9 +102,7 @@ class FastWorld:
         self.sizemat = self.entity_sizes[..., None] * self.entity_sizes[None, ...]
         
 
-        # World parameters
-        self.dt = 0.15
-        self.damping = 0.1
+        
 
     def step(self) -> None:
         angle: np.ndarray = np.array([np.cos(self.ctrl_thetas), np.sin(self.ctrl_thetas)])
@@ -263,7 +164,246 @@ class FastWorld:
         """
         self.positions =  np_random.unform(-1, +1, shape=(self.positions.shape))
         self.velocities[:] = 0
+        self.agent_actions[:] = 0
         
+
+
+
+class FastSimpleEnv:
+    def __init__(self,
+            scenario: FastScenario,
+            world: FastWorld,
+            max_cycles: int, 
+            continuous_actions: bool = False, 
+            local_ratio: bool = None):
+        
+        self.seed()
+        self.max_cycles = max_cycles
+        self.scenario = scenario
+        self.world = world
+        self.continuous_actions = continuous_actions
+        self.local_ratio = local_ratio
+
+        self.scenario.reset_world(self.world, self.np_random)
+        
+        self.action_spaces = None 
+        self.observation_spaces = None
+
+        self.possible_agents = self.world.agents.copy()
+        self.n_agents = self.world.agents.shape[0]
+
+        # TODO: Initialize action spaces
+
+    def observation_space(self, agent: int):
+        raise Exception("Observation space funciton not implemented")
+
+
+    def action_spaces(self, agent: int):
+        raise Exception("Action space funciton not implemented not implemented")
+    
+    def seed(self, seed: Optional[int] = None):
+        self.np_random, seed = seeding.np_random(seed)
+
+    
+    def observe(self, agent: int):
+        return self.scenario.observation(agent, self.world)
+    
+    def state(self):
+        raise Exception("State not implented yet...")
+    
+    def reset(self):
+        self.scenario.reset_world(self.world, self.np_random)
+
+        self.agents = self.possible_agents[:]
+        self.rewards = np.zeros(shape=self.agents.shape)
+        self._cumulative_rewards = np.zeros(shape=self.agents.shape)
+        
+        self.dones: np.ndarray[bool] = np.zeros(shape=self.agents.shape)
+        # I am omitting infos
+
+        self._reset_render()
+
+        # TODO: need to recall agent select reset
+        self.steps = 0 #Not sure what this does
+
+        # TODO: current_actions??
+        self.current_actions = [None] * self.n_agents
+
+    def _execute_world_step(self):
+        for i, agent in enumerate(self.world.agents):
+            action = self.current_actions[i]
+            scenario_action = []
+            # We don't need to ask if the agent is movable
+            mdim = self.world.dim_p * 2 + 1
+            # We don't do continuous actions
+
+            # Literally wtf is this
+            scenario_action.append(action % mdim)
+            action //= mdim
+
+            #if not agent.silent... we don't store that parameter
+            scenario_action.append(action)
+            # Need to fix this too
+            self._set_action(scenario_action, i, self.action_spaces[i])
+        
+        self.world.step()
+
+        self.rewards[:] = self.scenario.global_reward(self.world)
+        
+
+    def _set_action(self, action: List[int], agent_index: int, action_space, time=None)->None:
+        # All our agents are movable
+        # All our actions are discrete currently
+        if action[0] == 1:
+            self.world.agent_actions[agent_index][0] = 1.0
+        if action[0] == 2:
+            self.world.agent_actions[agent_index][0] = -1.0
+        if action[0] == 3:
+            self.world.agent_actions[agent_index][1] = -1.0
+        if action[0] == 4:
+            self.world.agent_actions[agent_index][1] = -1.0
+
+        # We do not currently have a concept of acceleration
+        sensitivity = 5.0
+        self.world.agent_actions[agent_index] *= sensitivity
+        action = action[1:]
+
+        # Our agents are never silent or continuous, and do not communicate
+
+        # To be honest I have no clue why we are passing what we are 
+        assert len(action) == 0
+
+
+    def step(self, action):
+        if self.dones[self.agent_selection]:
+            return self._was_done_step() # TODO: Implement this reference in AECEnv
+        
+        current_agent_idx = self.agent_selection
+        self.agent_selection = (current_agent_idx + 1) % self.n_agents
+        if self.agent_selection == 0:
+            self._execute_world_step()
+            self.steps += 1
+            if self.steps >= self.max_cycles:
+                self.dones[:] = True
+        else:
+            self._clear_rewards()
+        
+        #self._cumulative_rewards[cur_agent_idx] = 0
+        self._accumulate_rewards()
+
+
+
+    def _accumulate_rewards(self):
+        raise Exception(")_accumulative rewards not yet implemented")
+
+    def _clear_rewards(self):
+        raise Exception("_clear_rewards is not eyt implemented")
+
+
+    def _reset_render(self):
+        self.render_geoms = None
+        self.render_geoms_xform = None
+    def render(self, mode='human'):
+        from . import rendering
+
+        if self.viewer is None:
+            self.viewer = rendering.Viewer(700, 700)
+
+        # create rendering geometry
+        if self.render_geoms is None:
+            # import rendering only if we need it (and don't import for headless machines)
+            # from gym.envs.classic_control import rendering
+            # from multiagent._mpe_utils import rendering
+            self.render_geoms = []
+            self.render_geoms_xform = []
+            for entity in self.world.entities:
+                geom = rendering.make_circle(entity.size)
+                xform = rendering.Transform()
+                if 'agent' in entity.name:
+                    geom.set_color(*entity.color[:3], alpha=0.5)
+                else:
+                    geom.set_color(*entity.color[:3])
+                geom.add_attr(xform)
+                self.render_geoms.append(geom)
+                self.render_geoms_xform.append(xform)
+
+            # add geoms to viewer
+            self.viewer.geoms = []
+            for geom in self.render_geoms:
+                self.viewer.add_geom(geom)
+
+            self.viewer.text_lines = []
+            idx = 0
+            for agent in self.world.agents:
+                if not agent.silent:
+                    tline = rendering.TextLine(self.viewer.window, idx)
+                    self.viewer.text_lines.append(tline)
+                    idx += 1
+
+        alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        for idx, other in enumerate(self.world.agents):
+            if other.silent:
+                continue
+            if np.all(other.c == 0):
+                word = '_'
+            elif self.continuous_actions:
+                word = '[' + ",".join([f"{comm:.2f}" for comm in other.c]) + "]"
+            else:
+                word = alphabet[np.argmax(other.c)]
+
+            message = (other.name + ' sends ' + word + '   ')
+
+            self.viewer.text_lines[idx].set_text(message)
+
+        # update bounds to center around agent
+        all_poses = [entity.pos for entity in self.world.entities]
+        cam_range = np.max(np.abs(np.array(all_poses))) + 1
+        self.viewer.set_max_size(cam_range)
+        # update geometry positions
+        for e, entity in enumerate(self.world.entities):
+            self.render_geoms_xform[e].set_translation(*entity.pos)
+        # render to display or array
+        return self.viewer.render(return_rgb_array=mode == 'rgb_array')
+    
+    def close(self):
+        if self.viewer is not None:
+            self.viewer.close()
+            self.viewer = None
+        self._reset_render()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class raw_env(FastSimpleEnv):
+    def __init__(self, N=5, local_ratio=0.25, max_cycles=25, continuous_actions=True):
+        assert 0. <= local_ratio <= 1., "local_ratio is a proportion. Must be between 0 and 1."
+        scenario = FastScenario()
+        world = scenario.make_world(N)
+        super().__init__(scenario, world, max_cycles, continuous_actions, local_ratio)
+        self.metadata['name'] = "swarm_cover_v1"
+
+
+
 
 
 '''
