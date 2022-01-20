de70346398d3f95ffec1f235b798052e347ca7c6
Ryan Tse <rytse@umd.edu>
2022-01-20 17:07:23 -0500

    Remove old unused files

--- camoc_multiagent/envs/swarmcover/core.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-import pettingzoo.mpe._mpe_utils as mpe_utils
-
-
-class Target(mpe_utils.Landmark):
-    def __init__(self):
-        super().__init__()
-        self.size = 1.0  # targets are fat, we want to cover them
