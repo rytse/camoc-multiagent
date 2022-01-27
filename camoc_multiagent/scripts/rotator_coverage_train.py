6e415b8aeb1dae947c5dd812ed02681d9affadb9
Ryan Tse <rytse@umd.edu>
2022-01-26 20:18:53 -0500

    Use trainer script

--- camoc_multiagent/scripts/rotator_coverage_train.py
+++ camoc_multiagent/scripts/rotator_coverage_train.py
@@ -1,26 +1,31 @@
-from stable_baselines3.ppo import MlpPolicy
-from stable_baselines3 import PPO
 from envs.rotator_coverage import rotator_coverage_v0
+from rl_agent.ppo_utils import train_ppo
 
-env = rotator_coverage_v0.env_train()
-model = PPO(
-    MlpPolicy,
-    env,
-    verbose=3,
-    n_steps=2048,
+
+env_train = rotator_coverage_v0.env_train()
+env_eval = rotator_coverage_v0.env_train()
+
+model = train_ppo(
+    env_train,
+    env_eval,
+    "guess0_rotator_coverage_train",
+    total_timesteps=10_000_000,
+    n_steps=512,
     n_epochs=10,
-    batch_size=64,
-    gamma=0.95,
-    ent_coef=0.0905168,
-    learning_rate=0.00062211 * 10,
-    vf_coef=0.042202,
-    max_grad_norm=0.9,
+    batch_size=32,
+    gamma=0.995,
+    ent_coef=0.000217028956545287,
+    learning_rate=0.00398364348178975,
+    vf_coef=0.812838013528646,
+    max_grad_norm=5,
     gae_lambda=0.99,
-    clip_range=0.3,
+    clip_range=0.1,
+    sde_sample_freq=256,
+    policy_kwargs={
+        "log_std_init": -3.67097310146946,
+        "ortho_init": True,
+    },
 )
-
-model.learn(total_timesteps=100_000)
-model.save("./policies/rotator_coverage_ppo_policy")
 
 env = rotator_coverage_v0.env_eval()
 env.reset()
