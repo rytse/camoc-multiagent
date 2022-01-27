8c9851c68544f3c272910e22538868b28e675882
Ryan Tse <rytse@umd.edu>
2022-01-26 20:40:40 -0500

    Load hyperparams from json

--- camoc_multiagent/rl_agent/ppo_utils.py
+++ camoc_multiagent/rl_agent/ppo_utils.py
@@ -1,11 +1,13 @@
 from datetime import datetime
 
+import yaml
+
 from stable_baselines3.ppo import MlpPolicy
 from stable_baselines3 import PPO
 from stable_baselines3.common.callbacks import EvalCallback
 
 
-def train_ppo(env_train, env_eval, name, total_timesteps, **kwargs):
+def train_ppo(env_train, env_eval, name, total_timesteps, verbose=0, **kwargs):
     env_eval.reset()
 
     savebest_cb = EvalCallback(
@@ -21,7 +23,7 @@ def train_ppo(env_train, env_eval, name, total_timesteps, **kwargs):
         model = PPO.load(f"./policies/{name}")
         model.set_env(env_train)
     except Exception:
-        model = PPO(MlpPolicy, env_train, verbose=0, **kwargs)
+        model = PPO(MlpPolicy, env_train, verbose=verbose, **kwargs)
 
     model.learn(total_timesteps=total_timesteps, callback=savebest_cb)
     now = datetime.now()
@@ -40,3 +42,28 @@ def eval_ppo(env_eval, name):
         act = model.predict(obs, deterministic=True)[0] if not done else None
         env_eval.step(act)
         env_eval.render()
+
+
+def read_hyperparams(name):
+    hyperparams_dict = None
+    with open(f"rl_agent/hyperparams/{name}_hyp.json", "r") as f:
+        hyperparams_dict = yaml.safe_load(f)
+
+    ret_dict = {
+        "batch_size": hyperparams_dict["batch_size"],
+        "clip_range": hyperparams_dict["clip_range"],
+        "ent_coef": hyperparams_dict["ent_coef"],
+        "gae_lambda": hyperparams_dict["gae_lambda"],
+        "gamma": hyperparams_dict["gamma"],
+        "learning_rate": hyperparams_dict["learning_rate"],
+        "max_grad_norm": hyperparams_dict["max_grad_norm"],
+        "n_epochs": hyperparams_dict["n_epochs"],
+        "n_steps": hyperparams_dict["n_steps"],
+        "sde_sample_freq": hyperparams_dict["sde_sample_freq"],
+        "vf_coef": hyperparams_dict["vf_coef"],
+        "policy_kwargs": {
+            "log_std_init": hyperparams_dict["log_std_init"],
+            "ortho_init": True,
+        },
+    }
+    return ret_dict
--- camoc_multiagent/scripts/rotator_coverage_train.py
+++ camoc_multiagent/scripts/rotator_coverage_train.py
@@ -1,31 +1,15 @@
 from envs.rotator_coverage import rotator_coverage_v0
-from rl_agent.ppo_utils import train_ppo
+from rl_agent.ppo_utils import train_ppo, read_hyperparams
 
+NAME = "rotator_coverage_v0"
+NUM_TIMESTEPS = int(1e4)
 
 env_train = rotator_coverage_v0.env_train()
 env_eval = rotator_coverage_v0.env_train()
 
-model = train_ppo(
-    env_train,
-    env_eval,
-    "guess0_rotator_coverage_train",
-    total_timesteps=10_000_000,
-    n_steps=512,
-    n_epochs=10,
-    batch_size=32,
-    gamma=0.995,
-    ent_coef=0.000217028956545287,
-    learning_rate=0.00398364348178975,
-    vf_coef=0.812838013528646,
-    max_grad_norm=5,
-    gae_lambda=0.99,
-    clip_range=0.1,
-    sde_sample_freq=256,
-    policy_kwargs={
-        "log_std_init": -3.67097310146946,
-        "ortho_init": True,
-    },
-)
+hyp = read_hyperparams(NAME)
+
+model = train_ppo(env_train, env_eval, NAME, NUM_TIMESTEPS, verbose=3, **hyp)
 
 env = rotator_coverage_v0.env_eval()
 env.reset()
